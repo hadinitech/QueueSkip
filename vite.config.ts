0ae80830b98ecddcd2834502d1491576f8198e42
Glenda-Muthuhadini <glendamuthuhadini@gmail.com>
2026-04-27 21:28:37 +0200

    Update project files and CI/CD configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/QueueSkip/' : '/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
